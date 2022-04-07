dc744a0a026b8086f5c41a28a3378c7da9d7a7cb
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-04-07 13:51:58 +0200

    first commit

---
+++ src/main/java/com/gitexercises/GitexercisesApplication.java
@@ -0,0 +1,13 @@
+package com.gitexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitexercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gitexercises/GitexercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
