43b7f68a5ffa7ee7771e844f224598da13cbc344
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-05-03 20:07:47 +0200

    dupa test

--- src/main/java/com/gitexercises/ExampleClass.java
+++ src/main/java/com/gitexercises/ExampleClass.java
@@ -2,13 +2,12 @@ package com.gitexercises;
 
 public class ExampleClass {
     public String string;
-    public int number = 9;
-    public int number2 = 9;
+    public int number = 9;dfsgdfsgdfsg
     public int number3 = 9;
-    public int number4 = 9;
-    public int number5 = 9;
+    p
+    public int number5 = 9;dfs
     public int number6 = 9;
-    public int number7 = 9;
+    public int number7 = 9;dfsgdfsgdfsg
     dd
     tt
-}jghfjghfjghf
+}jghfjghfjghffdgdfs
